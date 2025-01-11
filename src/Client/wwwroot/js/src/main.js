b798ba43d5ac8cbe7ad16b8db4e6058e2239a9a6
zackmorgs <zackmorgenthaler@gmail.com>
2025-01-11 08:58:59 -0600

    added WebAPI, Models, and Client solutions. Configured webpack and SASS for good customization of bootstrap

---
+++ src/Client/wwwroot/js/src/main.js
@@ -0,0 +1,5 @@
+import * as bootstrap from 'bootstrap'
+
+import { Tooltip, Toast, Popover } from 'bootstrap'
+
+console.log("test 123");
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
-'use strict'
+'use strict';
 
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/main.js',
+  entry: './src/Client/wwwroot/js/src/main.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    path: path.resolve(__dirname, './src/Client/wwwroot/js/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // Process .scss files
+        use: [
+          MiniCssExtractPlugin.loader, // Extract CSS into a separate file
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles SCSS to CSS
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../styles/css/styles.css', // Output CSS file
+    }),
+  ],
+};
